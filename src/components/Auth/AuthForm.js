faf8692e10bf3949c34de4a6b5a91adabec2ddb1
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-23 22:38:32 +0530

    added the login functionality also

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,40 +19,50 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     // we could give some validation of email and password
-    
-    setisLoading(true);
-    if(isLogin){
 
-    }else{
-      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCO_5MwtpO6aijlQv3QHhntme1iMxUYmzs",
-      {
-        method:'POST',
-        body: JSON.stringify({
-          email:enteredEmail,
-          password:enteredPassword,
-          returnSecureToken:true
-        }),
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      }
-      ).then(res =>{
+    setisLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCO_5MwtpO6aijlQv3QHhntme1iMxUYmzs";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCO_5MwtpO6aijlQv3QHhntme1iMxUYmzs";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setisLoading(false);
-        if(res.ok){
+        if (res.ok) {
           // console.log("sent succesfullly");
-          alert("Account created succesful");
-        } else{
-            return res.json().then(data =>{
-            // console.log(data);
-            let errorMessage = 'Authrntication filed!';
-            if(data && data.error && data.error.message){
-               errorMessage = data.error.message;
+          // alert("Account created succesful");
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            console.log(data);
+            let errorMessage = "Authrntication filed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
@@ -73,8 +83,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-        {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
-        {isLoading && <p>Sending Request...</p>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
             className={classes.toggle}
